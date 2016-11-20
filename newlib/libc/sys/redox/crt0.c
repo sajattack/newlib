40baccd2d380f3acdcc13d08dfb93138bfbf2354
Jeremy Soller <jackpot51@gmail.com>
2016-11-20 09:07:55 -0700

    Fix argument handling

--- newlib/libc/sys/redox/crt0.c
+++ newlib/libc/sys/redox/crt0.c
@@ -1,7 +1,39 @@
-extern char ** environ;
-extern void exit(int code);
-extern int main(int argc, char ** argv, char ** envp);
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
 
-void _start() {
-    exit(main(0, 0, 0));
+extern char ** environ;
+extern int main(int argc, char ** argv);
+
+__attribute__((naked)) void _start() {
+    asm volatile(
+        ".intel_syntax noprefix;"
+        "xchg bx, bx;"
+        "add rsp, 0x8;" //Undo GCC's alignment
+        "mov rdi, rsp;"
+        "and rsp, 0xFFFFFFFFFFFFFFF0;"
+        "call _start_stack;"
+        ".att_syntax prefix;"
+        : : : "memory");
+    exit(0);
+}
+
+struct slice {
+    uint64_t len;
+    char * ptr;
+} __attribute__((packed));
+
+void _start_stack(uint64_t * stack) {
+    int argc = (int)(stack[0]);
+    struct slice * rust_argv = (struct slice *)(&stack[1]);
+
+    char ** argv = calloc(argc, sizeof(char *));
+    int i;
+    for(i = 0; i < argc; i++){
+        struct slice rust_arg = rust_argv[i];
+        char * arg = calloc(rust_arg.len + 1, 1);
+        memcpy(arg, rust_arg.ptr, rust_arg.len);
+        argv[i] = arg;
+    }
+    exit(main(argc, argv));
 }
