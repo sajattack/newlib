68fbe4f5026862539508027030426139ed188e6e
Ian Douglas Scott <ian@iandouglasscott.com>
2017-04-14 21:26:23 -0700

    Implement chmod

--- newlib/libc/sys/redox/file.c
+++ newlib/libc/sys/redox/file.c
@@ -95,3 +95,7 @@ int _unlink(const char *path) {
 int _write(int file, const char *ptr, int len) {
     return syscall3(SYS_WRITE, (uint64_t)file, (uint64_t)ptr, (uint64_t)len);
 }
+
+int chmod(const char * path, mode_t mode) {
+    return syscall3(SYS_CHMOD, (uint64_t)path, (uint64_t)strlen(path), (uint64_t)mode);
+}
