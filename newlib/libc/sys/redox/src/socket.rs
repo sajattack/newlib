d162585b50978c034a0da2796abee8ab01e115cc
Ian Douglas Scott <ian@iandouglasscott.com>
2017-06-16 14:20:45 -0700

    Add sendfrom() and recvfrom()

--- newlib/libc/sys/redox/src/socket.rs
+++ newlib/libc/sys/redox/src/socket.rs
@@ -1,6 +1,6 @@
 use core::str::{self, FromStr};
 use ::types::{c_int, c_char, size_t, c_void, ssize_t, socklen_t, in_addr, sockaddr, sockaddr_in};
-use syscall::{self, O_RDWR};
+use syscall::{self, O_RDWR, O_WRONLY};
 use syscall::error::{Error, EPROTOTYPE, EPROTONOSUPPORT, EAFNOSUPPORT, EINVAL, EOPNOTSUPP};
 use core::slice;
 use collections::Vec;
@@ -86,6 +86,31 @@ libc_fn!(unsafe send(socket: c_int, buffer: *const c_void, length: size_t, flags
     } else {
         let buf = slice::from_raw_parts(buffer as *const u8, length);
         Ok(syscall::write(socket as usize, buf)? as ssize_t)
+    }
+});
+
+libc_fn!(unsafe recvfrom(socket: c_int, buffer: *mut c_void, length: size_t, flags: c_int, address: *const sockaddr, _address_len: *const socklen_t) -> Result<ssize_t>  {
+    let fd = syscall::dup(socket as usize, b"listen")?;
+    let mut path = [0; 4096];
+    syscall::fpath(socket as usize, &mut path)?;
+    // XXX process path and write to address
+    let ret = recv(socket, buffer, length, flags);
+    syscall::close(fd)?;
+    Ok(ret)
+});
+
+libc_fn!(unsafe sendto(socket: c_int, message: *const c_void, length: size_t, flags: c_int, dest_addr: *const sockaddr, _dest_len: socklen_t) -> Result<ssize_t> {
+    // XXX test dest_len
+    if (*dest_addr).sa_family as i32 == AF_INET {
+        let addr = &*(dest_addr as *const sockaddr_in);
+        let s_addr = addr.sin_addr.s_addr;
+        let url = format!("{}.{}.{}.{}:{}", s_addr[0], s_addr[1], s_addr[2], s_addr[3], ntohs(addr.sin_port));
+        let fd = syscall::dup(socket as usize, url.as_bytes())?;
+        let ret = send(fd as c_int, message, length, flags);
+        syscall::close(fd)?;
+        Ok(ret)
+    } else {
+        Err(Error::new(EOPNOTSUPP))
     }
 });
 
