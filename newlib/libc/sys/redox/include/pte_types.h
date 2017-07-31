3cb28f2926e35b5918788176fa3b9c8cdb29224a
Ian Douglas Scott <ian@iandouglasscott.com>
2017-07-31 10:22:34 -0700

    Add some code for pthreads

---
+++ newlib/libc/sys/redox/include/pte_types.h
@@ -0,0 +1,7 @@
+#ifndef PTE_TYPES_H
+#define PTE_TYPES_H
+
+#include <errno.h>
+#include <sys/types.h>
+
+#endif /* PTE_TYPES_H */
---
+++ newlib/libc/sys/redox/pte_osal.h
@@ -0,0 +1,11 @@
+#ifndef _OS_SUPPORT_H_
+#define _OS_SUPPORT_H_
+
+// XXX
+typedef unsigned long pte_osThreadHandle;
+typedef unsigned long pte_osSemaphoreHandle;
+typedef unsigned long pte_osMutexHandle;
+
+#include "pthreads-emb/pte_generic_osal.h"
+
+#endif // _OS_SUPPORT_H
