e6c24a0f0e25a34f0a3227ce4c53cba5b6cb5cc6
Ian Douglas Scott <ian@iandouglasscott.com>
2017-07-31 15:31:09 -0700

    Commit pte_osal.h

--- newlib/libc/sys/redox/pte_osal.h
+++ newlib/libc/sys/redox/pte_osal.h
@@ -1,10 +1,12 @@
 #ifndef _OS_SUPPORT_H_
 #define _OS_SUPPORT_H_
 
+#include <sys/types.h>
+
 // XXX
 typedef unsigned long pte_osThreadHandle;
 typedef unsigned long pte_osSemaphoreHandle;
-typedef unsigned long pte_osMutexHandle;
+typedef int32_t* pte_osMutexHandle;
 
 #include "pthreads-emb/pte_generic_osal.h"
 
